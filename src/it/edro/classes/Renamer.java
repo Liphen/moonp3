e814d4fb37c535a14d3a073be04b5bc82e957c9d
Liphen <adriano.zamprotta@gmail.com>
2015-09-15 19:14:45 +0200

    Migliorata l'interfaccia (aggiunto il scroll) - Aggiunto il file di log - Aggiunto il TODO - Iniziato la classe Renamer

--- src/it/edro/classes/Renamer.java
+++ src/it/edro/classes/Renamer.java
@@ -1,5 +1,30 @@
 package it.edro.classes;
 
-public class Renamer {
+import java.io.File;
 
+import javax.swing.JTextArea;
+
+import org.apache.log4j.Logger;
+
+public class Renamer {
+	
+	private static Logger log = Logger.getLogger(Renamer.class);
+	
+	public static void main(File directory, JTextArea textArea){
+		File listFile[] = directory.listFiles();
+		int fileNumber = 0; 
+		
+		if(listFile != null){
+			
+			log.debug("Renamer iniziato");
+			
+			for(int i = 0; i < listFile.length; i++){
+				textArea.append(fileNumber++ + " " + listFile[i].getName() + "\n");
+				log.info("Sto cercando il file numero " + fileNumber + " con il nome: " + listFile[i].getName());
+				
+			}		
+			
+			log.debug("Renamer finito");
+		}
+	}
 }
--- src/it/edro/gui/Interface.java
+++ src/it/edro/gui/Interface.java
@@ -5,11 +5,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JProgressBar;
 import javax.swing.JButton;
-import javax.swing.JTextPane;
+import javax.swing.JFileChooser;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.EmptyBorder;
+
+import org.apache.log4j.PropertyConfigurator;
+
+import it.edro.classes.Renamer;
 
 public class Interface {
 
@@ -18,7 +28,11 @@ public class Interface {
 	private JButton btnRetag;
 	private JButton btnRename;
 	private JButton btnStartBoth;
-	private JTextPane textPane;
+	private JButton btnBrowse;
+	private	JTextArea textArea;
+	private JScrollPane scrollPane;
+	
+	private JFileChooser chooser;
 
 	/**
 	 * Launch the application.
@@ -54,32 +68,81 @@ public class Interface {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		PropertyConfigurator.configure("log/log4j.xml");
+		
 		frmMoonp = new JFrame();
 		frmMoonp.setResizable(false);
 		frmMoonp.setIconImage(Toolkit.getDefaultToolkit().getImage(Interface.class.getResource("/com/sun/javafx/webkit/prism/resources/mediaPlayDisabled.png")));
 		frmMoonp.setTitle("Moonp3");
-		frmMoonp.setBounds(100, 100, 443, 291);
+		frmMoonp.setBounds(100, 100, 458, 291);
 		frmMoonp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmMoonp.getContentPane().setLayout(null);
 		
 		progressBar = new JProgressBar();
-		progressBar.setBounds(6, 236, 422, 19);
+		progressBar.setBounds(6, 236, 440, 19);
 		frmMoonp.getContentPane().add(progressBar);
 		
 		btnRetag = new JButton("Retag");
-		btnRetag.setBounds(338, 196, 90, 28);
+		btnRetag.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnRetag.setBounds(356, 197, 90, 28);
 		frmMoonp.getContentPane().add(btnRetag);
 		
 		btnRename = new JButton("Rename");
-		btnRename.setBounds(338, 156, 90, 28);
+		btnRename.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (chooser == null)
+					textArea.append("\n Nessuna directory scelta\n");
+				else
+					Renamer.main(chooser.getSelectedFile(), textArea);
+			}
+		});
+		btnRename.setBounds(356, 158, 90, 28);
 		frmMoonp.getContentPane().add(btnRename);
 		
 		btnStartBoth = new JButton("Start Both");
-		btnStartBoth.setBounds(338, 116, 90, 28);
+		btnStartBoth.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnStartBoth.setBounds(356, 119, 90, 28);
 		frmMoonp.getContentPane().add(btnStartBoth);
 		
-		textPane = new JTextPane();
-		textPane.setBounds(6, 6, 320, 218);
-		frmMoonp.getContentPane().add(textPane);
+		//Serve per prendere la directory
+		btnBrowse = new JButton("Browse");
+		btnBrowse.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				chooser = new JFileChooser();
+			    chooser.setCurrentDirectory(new java.io.File(System.getProperty("user.home")));
+			    chooser.setDialogTitle("Mp3s' directory");
+			    chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			    chooser.setAcceptAllFileFilterUsed(false);
+
+			    if (chooser.showOpenDialog(frmMoonp) == JFileChooser.APPROVE_OPTION) {
+			    	textArea.append(" La directory selezionata è: \n " + chooser.getSelectedFile() + "\n");
+			    } else {
+			    	textArea.append(" Directory non selezionata" + "\n");
+			    }
+			}
+		});
+		btnBrowse.setBounds(357, 11, 89, 23);
+		frmMoonp.getContentPane().add(btnBrowse);
+
+		textArea = new JTextArea();
+		textArea.setEditable(false);
+		textArea.setBounds(6, 10, 322, 215);
+		textArea.setText("- Selezionare la cartella che contiene gli mp3\n");
+		textArea.setLineWrap(true);
+//		frmMoonp.getContentPane().add(textArea);
+		
+		scrollPane = new JScrollPane(textArea);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setBounds(6, 10, 322, 215);
+		frmMoonp.getContentPane().add(scrollPane);
+		
 	}
 }
